0b0142fefc2c686a342d4d23e93e2a820434c1b3
邱军 <qiujungaoaiming@hotmail.com>
2018-05-18 00:42:46 +0800

    做的作业
    这一版本是作业

---
+++ pages/index/index.js
@@ -0,0 +1,37 @@
+Page({
+
+  data: {
+    array: ["国内", "国际", "财经", "娱乐", "军事", "体育", "其他"],
+    num:0,
+    news:[]    
+   },
+
+  onLoad: function (option) {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list', // 这里写请求的url
+      data: {
+        type: 'gn' // 此处为请求时候的数据
+      },
+      success: res => {
+        // 数据请求成功时候的处理代码
+        // console.log(res.data)
+        let result = res.data.result
+        this.setData({
+          news:result
+        })
+      },
+      onTapdetail() {
+        wx.navigateTo({
+          url: 'pages/detail/detail?id=1523074607642'
+        })
+      },
+      complete: () => {
+        // 数据请求完成时候的处理代码
+      }
+    })
+  },
+  tabClick(e) {
+    var num = e.currentTarget.dataset.index
+    this.setData({ num })
+  }
+})
